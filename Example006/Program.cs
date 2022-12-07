149041a866ba714488e682df450f199875b71b58
Vkapeschin <kapeshin-vladimir@rambler.ru>
2022-12-08 00:20:54 +0300

    Example006

--- Example006/Program.cs
+++ Example006/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a = 2;
+int b = 22;
+int c = 9;
+int d = 3;
+int e = 28;
+
+int max = a;
+
+if (max < b) max = b;
+if (max < c) max = c;
+if (max < d) max = d;
+if (max < e) max = e;
+
+Console.WriteLine(max);

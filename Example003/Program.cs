18367c46455748b9bb52ae1da52603cfb25fa90a
Vkapeschin <kapeshin-vladimir@rambler.ru>
2022-12-07 23:18:02 +0300

    Example003 done

--- Example003/Program.cs
+++ Example003/Program.cs
@@ -1,5 +1,5 @@
 ﻿int numberA = 4;
-Console.WriteLine(numberA);
 int numberB = 3;
-Console.WriteLine(numberB);
-Console.WriteLine(numberA + numberB);
+int result = numberA + numberB;
+Console.Write("Результат = ");
+Console.WriteLine(result);
